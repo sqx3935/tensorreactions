c4a9301c285d62a296402e2b6405ef6ea7f024a5
nil <nil2share@gmail.com>
2020-03-27 09:27:44 -0400

    symbolic setup script

--- Nil Reaction Library/setup.ps1
+++ Nil Reaction Library/setup.ps1
@@ -1,18 +1,17 @@
+#Requires -RunAsAdministrator
 # only works if you follow the setup structure I layed out in https://github.com/nil2share/tensorreactions/wiki/How-to-setup-git-for-easier-updates
 
 # setup lib for library
-#New-Item -f -ItemType SymbolicLink -Path "C:\MINIONAPP\Bots\FFXIVMinion64\LuaMods\Nil Reaction Library" -Target "C:\Projects\src\github.com\nil2share\tensorreactions\Nil Reaction Library"
+New-Item -f -ItemType SymbolicLink -Path "C:\MINIONAPP\Bots\FFXIVMinion64\LuaMods\Nil Reaction Library" -Target "C:\Projects\src\github.com\nil2share\tensorreactions\Nil Reaction Library"
 
 # loop through timeline and seutp symbolic link
 Get-ChildItem "C:\Projects\src\github.com\nil2share\tensorreactions\TimelineTriggers" -Filter *.lua | 
 Foreach-Object {
-    #$content = Get-Content $_.FullName
-    Write-Host $_.FullName
-    Write-Host $_.Name
-    New-Item -f -ItemType SymbolicLink -Path "C:\Temp\test\"+ $_.Name -Target $_.FullName
-    #filter and save content to the original file
-   # $content | Where-Object {$_ -match 'step[49]'} | Set-Content $_.FullName
+    New-Item -f -ItemType SymbolicLink -Path "C:\MINIONAPP\Bots\FFXIVMinion64\LuaMods\TensorReactions\TimelineTriggers\$_" -Target $_.FullName
+}
 
-    #filter and save content to a new file 
-   # $content | Where-Object {$_ -match 'step[49]'} | Set-Content ($_.BaseName + '_out.log')
+# loop through general and seutp symbolic link
+Get-ChildItem "C:\Projects\src\github.com\nil2share\tensorreactions\GeneralTriggers" -Filter *.lua | 
+Foreach-Object {
+    New-Item -f -ItemType SymbolicLink -Path "C:\MINIONAPP\Bots\FFXIVMinion64\LuaMods\TensorReactions\GeneralTriggers\$_" -Target $_.FullName
 }
